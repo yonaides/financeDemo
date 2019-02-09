f5cebe72c31d5666a0dbece044c3768fc72b2ef7
yonaides <yonaides@gmail.com>
2019-02-08 20:24:43 -0400

    Add project files.

---
+++ FinanceMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinanceMVC.MvcApplication" Language="C#" %>
---
+++ FinanceMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceMVC.Startup))]
+namespace FinanceMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
