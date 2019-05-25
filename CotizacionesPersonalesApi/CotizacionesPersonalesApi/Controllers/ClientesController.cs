0d81f3c8f508d5c8482af376b01c219539c6b307
Yonaides Tavares <yonaides@gmail.com>
2019-05-25 19:23:40 -0400

    primer commit

---
+++ CotizacionesPersonalesApi/CotizacionesPersonalesApi/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CotizacionesPersonalesApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+
+        [HttpGet(Name = nameof(GetClientes))]
+        public IActionResult GetClientes()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ CotizacionesPersonalesApi/CotizacionesPersonalesApi/Controllers/RootController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CotizacionesPersonalesApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        [ProducesResponseType(200)]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(ClientesController.GetClientes), null)
+                }
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
